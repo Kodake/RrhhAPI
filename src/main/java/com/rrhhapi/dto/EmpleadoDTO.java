c2664302094fba0938e49f272cea7665fd461eeb
Kodake <kodake@hotmail.es>
2024-04-11 21:44:27 -0400

    Using modelMapper

---
+++ src/main/java/com/rrhhapi/dto/EmpleadoDTO.java
@@ -0,0 +1,2 @@
+package com.rrhhapi.dto;public class EmpleadoDTO {
+}
---
+++ src/main/java/com/rrhhapi/dto/GetEmpleadoDTO.java
@@ -0,0 +1,11 @@
+package com.rrhhapi.dto;
+
+import lombok.Data;
+
+@Data
+public class EmpleadoDTO {
+    Integer idEmpleado;
+    String nombre;
+    String departamento;
+    Double sueldo;
+}
